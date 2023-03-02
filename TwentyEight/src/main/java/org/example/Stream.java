48f8c1dd11357ceed5abcbd34337102fc949b80c
DheerajKumar19 <dmohanku@uci.edu>
2023-03-01 19:33:16 -0800

    Week 7- code clean up and documentation

--- TwentyEight/src/main/java/org/example/Stream.java
+++ TwentyEight/src/main/java/org/example/Stream.java
@@ -13,13 +13,28 @@ import java.util.stream.Collectors;
 
 public class Stream {
     public static void main(String[] args) throws IOException {
+        /**
+         * using two args to load 2 files
+         * Uri loader load my class paths
+         */
         try {
             URI uri1 = ClassLoader.getSystemResource(args[0]).toURI();
             URI uri2 = ClassLoader.getSystemResource(args[1]).toURI();
 
+            /**
+             * this loads the stop words from file into a list
+             */
             List<String> stop_words = Files.lines(Paths.get(uri1))
                     .flatMap(line -> Arrays.stream(line.split(","))).collect(Collectors.toList());
 
+            /**
+             * this will first load the pride and prejudice file
+             * get it as a stream and splits based on the regex
+             * puts into a map filtered based on stopwords and length
+             * then we do a sort on this (descending)
+             * set limit or get first 25
+             * print them
+             */
             Files.lines(Paths.get(uri2))
                     .flatMap(line -> Arrays.stream(line.split("[^a-zA-Z0-9]+")))
                     .map(String::toLowerCase)
